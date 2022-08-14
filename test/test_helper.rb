57877f99960e81596526e7de2c2c77c82513c7cb
David Crosby <dave@dafyddcrosby.com>
2022-08-14 16:12:01 -0600

    Require dc_minitest for general minitest config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
+require "dc_minitest"
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "coffeeoutside"
-
-require "minitest/autorun"
-require "minitest/pride"
